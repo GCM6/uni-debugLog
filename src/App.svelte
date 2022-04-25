3a5ce012b2e323b2980cbab11795e6230f331887
MingIce <1758733658@qq.com>
2022-04-25 22:32:10 +0800

    feat:添加浮标功能

--- src/App.svelte
+++ src/App.svelte
@@ -11,69 +11,68 @@
   const onChangeTab = (e: any) => {
     targetLi = e.target.id;
   };
-
+  let switchOpen: boolean = true;
   const clearStore = () => {
     clearStatus = true;
   };
+  const changePanel = (status: boolean) => {
+    switchOpen = status;
+  };
 </script>
 
-<div class="flat-tag">
-  <span class="log-root">
-    <span class="log-dot">
-      <span class="switch-thumb" />
+{#if switchOpen}
+  <div class="flat-tag" on:click={() => changePanel(false)}>
+    <span class="log-root">
+      <span class="log-dot"> LOG </span>
+      <span class="switch-track" />
     </span>
-    <span class="switch-track" />
-  </span>
-</div>
-<div class="log-page">
-  <div class="app-continer">
-    <div class="tab">
-      <ul class="tab-continer" on:click={onChangeTab}>
-        <li id="li1" class="item" class:active={targetLi === "li1"}>Log</li>
-        <li id="li2" class="item" class:active={targetLi === "li2"}>network</li>
-        <li id="li3" class="item" class:active={targetLi === "li3"}>storage</li>
-      </ul>
-    </div>
-    <div class="main">
-      <!-- 用 $前缀来存储store值 -->
-      {#if targetLi === "li1"}
-        <div class="log-continer">
-          <Log bind:clearStatus />
-        </div>
-      {:else if targetLi === "li2"}
-        <div class="network-continer">
-          <Network />
-        </div>
-      {:else if targetLi === "li3"}
-        <div class="network-continer">storage</div>
-      {/if}
-    </div>
-    <div class="btn-container">
-      <div class="btn clear-btn" on:click={clearStore}>clear Log</div>
-      <div class="btn">Hide page</div>
-      <div />
-    </div>
   </div>
-  <div class="app-mask-layer" />
-</div>
+{:else}
+  <div class="log-page">
+    <div class="app-continer">
+      <div class="tab">
+        <ul class="tab-continer" on:click={onChangeTab}>
+          <li id="li1" class="item" class:active={targetLi === "li1"}>Log</li>
+          <li id="li2" class="item" class:active={targetLi === "li2"}>
+            network
+          </li>
+          <li id="li3" class="item" class:active={targetLi === "li3"}>
+            storage
+          </li>
+        </ul>
+      </div>
+      <div class="main">
+        <!-- 用 $前缀来存储store值 -->
+        {#if targetLi === "li1"}
+          <div class="log-continer">
+            <Log bind:clearStatus />
+          </div>
+        {:else if targetLi === "li2"}
+          <div class="network-continer">
+            <Network />
+          </div>
+        {:else if targetLi === "li3"}
+          <div class="network-continer">storage</div>
+        {/if}
+      </div>
+      <div class="btn-container">
+        <div class="btn clear-btn" on:click={clearStore}>clear Log</div>
+        <div class="btn">Hide page</div>
+        <div />
+      </div>
+    </div>
+    <div class="app-mask-layer" on:click={() => changePanel(true)} />
+  </div>
+{/if}
 
 <style>
-  .switch-thumb {
-    box-sizing: border-box;
-    width: 44px;
-    height: 44px;
-    box-shadow: 0px 2px 1px -1px rgb(0 0 0 / 20%),
-      0px 1px 1px 0px rgb(0 0 0 / 14%), 0px 1px 3px 0px rgb(0 0 0 / 12%);
-    background-color: currentColor;
-    border-radius: 50%;
-  }
   .switch-track {
     background-color: rgb(46, 202, 69);
     opacity: 0.5;
     border: 0px;
     width: 100%;
     height: 100%;
-    border-radius: 24px;
+    border-radius: 32px;
   }
   .log-dot {
     display: inline-flex;
@@ -86,13 +85,14 @@
     vertical-align: middle;
     appearance: none;
     text-decoration: none;
-    padding: 20px;
     border-radius: 50%;
     position: absolute;
-    top: 0px;
-    left: 0px;
+    top: 50%;
+    right: 0px;
     z-index: 1;
-    color: rgb(224, 224, 224);
+    transform: translate(-50%, -50%);
+    color: white;
+    text-align: center;
   }
   .log-root {
     display: inline-flex;
@@ -103,7 +103,7 @@
     z-index: 0;
     vertical-align: middle;
     width: 120px;
-    height: 48px;
+    height: 56px;
     padding: 0px;
     margin: 16px;
   }
@@ -180,7 +180,6 @@
   }
 
   .app-mask-layer {
-    display: none;
     z-index: 998;
     position: fixed;
     left: 0;
