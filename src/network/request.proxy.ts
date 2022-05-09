bd73c60717fc379d039ce9bf2b4c7da00e2042c3
MingIce <1758733658@qq.com>
2022-05-09 22:54:26 +0800

    feat:编写请求的代理

--- src/network/request.proxy.ts
+++ src/network/request.proxy.ts
@@ -1,4 +1,46 @@
 import { NetworkItemDetail } from "./requestItemDetail";
+// 泛型约束
+class XmlReqHandler<T extends XMLHttpRequest> {
+  private reqItemDetail: NetworkItemDetail;
+  constructor(xhr: XMLHttpRequest, callback: Function) {
+    this.reqItemDetail = new NetworkItemDetail();
+  }
+  public get(target: T, key: string) {
+    switch (key) {
+      case "open":
+        return this.getOpenData(target);
+
+      case "send":
+        return this.getSendData(target);
+
+      case "setRequestHeader":
+        return this.getSetRequestHeader(target);
+      default:
+        const xhrVal = Reflect.get(target, key);
+        if (typeof xhrVal === "function") {
+          return xhrVal.bind(target);
+        } else {
+          return xhrVal;
+        }
+    }
+  }
+  public set(target: T, key: string, val: any) {
+    return Reflect.set(target, key, val);
+  }
+  private getOpenData(target: T) {
+    return (...args: any) => {
+      console.log("args", args);
+      // 设置请求类型默认值
+      const requestMethod = args[0] ? args[0].toUpperCase() : "GET";
+      this.reqItemDetail.method = requestMethod;
+      // TODO
+    };
+  }
+  private getSendData(target: T) {
+  }
+  private getSetRequestHeader(target: T) {
+  }
+}
 export class requestProxy {
   // 保持默认方式
   public static origXHR = XMLHttpRequest;
@@ -7,9 +49,7 @@ export class requestProxy {
     return new Proxy(XMLHttpRequest, {
       construct(XHR) {
         const XMLReq = new XHR();
-        console.log("----------->", XMLReq);
-        // TODO
-        return XMLReq;
+        return new Proxy(XMLReq, new XmlReqHandler(XMLReq, callback));
       },
     });
   }
